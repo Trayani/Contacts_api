927a270daf4244c64026151bab63f656dbbd0a20
Jan Psota <jan.psota@budgetbakers.com>
2023-07-31 19:47:31 +0200

    improved resource handling

--- src/main/java/cz/fio/contacts/StoreContactService.java
+++ src/main/java/cz/fio/contacts/StoreContactService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.Closeable;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -32,29 +33,33 @@ public class StoreContactService {
     }
 
 
-    /** Finds existing contacts record or stores a new one if it already exists
+    /**
+     * Finds existing contacts record or stores a new one if it already exists
+     *
      * @param contactCsvRow a full contact record in CSV-line format
-     * */
+     */
     public ContactStorageResult findOrStoreContact(String contactCsvRow) {
         return findOrStoreContact(contactCsvRow, true);
     }
 
 
-    /** Finds existing contacts record or stores a new one if it already exists
-     * @param contactCsvRow a full contact record in CSV-line format
-     * @param retryOnNonExistingFile
-     * if <b>true</b> and in case of @FileNotFound exception, a the contacts file will be created anew
-     * and the contact record will be stored to it
-     * */
+    /**
+     * Finds existing contacts record or stores a new one if it already exists
+     *
+     * @param contactCsvRow          a full contact record in CSV-line format
+     * @param retryOnNonExistingFile if <b>true</b> and in case of @FileNotFound exception, a the contacts file will be created anew
+     *                               and the contact record will be stored to it
+     */
     private ContactStorageResult findOrStoreContact(String contactCsvRow, boolean retryOnNonExistingFile) {
+        RandomAccessFile file = null;
+        FileChannel channel = null;
+        FileLock lock = null;
         try {
             synchronized (fileLock) {
-                RandomAccessFile file = new RandomAccessFile(config.getContactsFilePath(), "rw");
-                FileChannel channel = file.getChannel();
-                FileLock lock = channel.lock();
-                try (file; channel; lock) {
-                    return findOrStoreContactInternal(contactCsvRow, file);
-                }
+                file = new RandomAccessFile(config.getContactsFilePath(), "rw");
+                channel = file.getChannel();
+                lock = channel.lock();
+                return findOrStoreContactInternal(contactCsvRow, file);
             }
         } catch (Throwable e) {
             if (e instanceof FileNotFoundException && retryOnNonExistingFile) {
@@ -66,30 +71,49 @@ public class StoreContactService {
             }
             logger.error("Error during contact record lookup", e);
             return new ContactStorageResult(StorageResultType.INTERNAL_ERROR, "Error during contact record lookup");
+        } finally {
+            closeQuietly(lock);
+            closeQuietly(channel);
+            closeQuietly(file);
         }
+    }
 
+    /** attempts to close a resource;
+     *  - if the resource value is null, nothing happens
+     *  - if the an error occurs, it is ignore (TODO: monitoring of closing errors ) */
+    private void closeQuietly(AutoCloseable closeable) {
+        if (closeable == null)
+            return;
+        try {
+            closeable.close();
+        } catch (Throwable ignored) {
+        }
     }
 
 
-    /** Creates a new contacts CSV file amd proceeds to store new record to it
+    /**
+     * Creates a new contacts CSV file amd proceeds to store new record to it
+     *
      * @param contactCsvRow a full contact record in CSV-line format
-     * */
+     */
     private ContactStorageResult createNewContactsFileAndStore(String contactCsvRow) throws IOException {
         logger.warn("Contacts file not found. Creating new one...");
         java.io.File targetFile = new java.io.File(config.getContactsFilePath());
         java.io.File parent = targetFile.getParentFile();
         if (!(parent != null && !parent.exists() && !parent.mkdirs())) {
             logger.warn("New contacts file created");
-            return findOrStoreContact(contactCsvRow);
+            return findOrStoreContact(contactCsvRow, false);
         }
         throw new IOException("Could not create contacts file");
     }
 
 
-    /** Scans the contacts file. If not found, record will be stored
+    /**
+     * Scans the contacts file. If not found, record will be stored
+     *
      * @param contactCsvRow a full contact record in CSV-line format
-     * @param file an object representing the contacts file within the file system
-     * */
+     * @param file          an object representing the contacts file within the file system
+     */
     private ContactStorageResult findOrStoreContactInternal(String contactCsvRow, RandomAccessFile file) throws IOException {
         var lineIndex = 0;
 
@@ -105,7 +129,9 @@ public class StoreContactService {
     }
 
 
-    /** Evaluates contact line match. Con be configured for case-insensitive match     */
+    /**
+     * Evaluates contact line match. Con be configured for case-insensitive match
+     */
     private boolean compareContactLine(String exstingRecordLine, String lookupCsvLine) {
         if (config.isCaseInsensitiveContactMatch())
             return exstingRecordLine.equalsIgnoreCase(lookupCsvLine);
@@ -113,7 +139,9 @@ public class StoreContactService {
     }
 
 
-    /** Stores new record to the contacts file  */
+    /**
+     * Stores new record to the contacts file
+     */
     private ContactStorageResult storeNewContact(String expectedCsvRow, RandomAccessFile file) {
         try {
             var newLineExists = true;
